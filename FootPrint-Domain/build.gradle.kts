581895b48c509882cf5a4a8712bd135707c47261
jifrozen0110 <answldjs1836@gmail.com>
2024-06-07 11:24:32 +0900

    #1 ci workflow 생성, 통합 테스트를 위한 testcontainers 도입 (#2)
    * chore : testContainers 추가 (#1)
    
    통합 테스트를 위해 인프라 환경과 동일한 testContainers를 구성하여 테스트 멱등성을 보장합니다.
    
    * chore : ci workflow 추가 (#1)
    
    * test : workflow 테스트 (#1)
    
    * test : workflow 테스트 (#1)
    
    * test : workflow 테스트 (#1)

---
+++ FootPrint-Domain/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+
+    // testContainers
+    testImplementation("org.testcontainers:testcontainers:1.19.8")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.8")
+    testImplementation("org.testcontainers:oracle-xe:1.19.8")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    // DB 설정
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.21.0.0")
+}
+
+// kotlin으로 JPA를 사용할때 필요한 설정
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
